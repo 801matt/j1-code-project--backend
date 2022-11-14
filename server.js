e6d0ee7feab645013803bfffa5d6126dc173f35c
Matt Jackson <matt@Matts-MacBook-Pro.local>
2022-11-14 11:01:20 -0700

    Create express server. Install dependencies for proper api requests, handling, cors

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const breaches = require("./Routes/breaches");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(breaches);
+
+app.listen(4000, () => console.log("Example app is listening on port 4000."));
